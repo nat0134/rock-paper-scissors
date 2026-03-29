b9c4319f756c4698051000c166a694bd53022d4c
Natalia Lee <z2191119@ad.unsw.edu.au>
2026-03-30 01:37:45 +1100

    Wrote logic for full game moved score variables and beats object along with playRound() function moved inside playGame function to maintain scope

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 /**
+ * The program prompts a user to input a choice to play with the computer of the
+ * game "Rock Paper Scissors", winners and losers are announced. Each round of 
+ * game records accumulated scores. When full game is finalised, final winner 
+ * and score is announced.
  * 
+ * Written by "Natalia Lee"
+ * Date: 29/03/2026
  */
 
-let humanScore = 0;
-let computerScore = 0;
-const humanRound = getHumanChoice().toLowerCase();
-const computerRound = getComputerChoice().toLowerCase();
-const beats = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper"
-};
-
 /**
- * Function uses Math.random() to generate and return one of the string values
- * "rock", "paper" or "scissors".
+ * Uses Math.random() to generate and return one of the string values
+ * "rock", "paper" or "scissors" as computer's choice.
+ * 
+ * @returns {string} The computer's choice is returned.
  */
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
@@ -29,11 +27,15 @@ function getComputerChoice() {
 }
 
 /**
- * Function prompts player to enter one of the strings "rock", "paper" or "scissors" 
- * and returns the chosen string. Invalid inputs are not required to be handled.
+ * Human player is prompted to enter one of the strings "rock", "paper" or 
+ * "scissors" and the player's choice is return in string. Invalid inputs are 
+ * not required to be handled.
+ * 
+ * @returns {string} The human layer's input is returned.
  */
 function getHumanChoice() {
-    let playerChoice = prompt("Make a choice!").toLowerCase();
+    let playerChoice = prompt("Make a choice - paper, scissors or rock!")
+                       .toLowerCase();
 
     if (playerChoice === "rock") {
         return "rock";
@@ -45,21 +47,50 @@ function getHumanChoice() {
 }
 
 /**
+ * Plays full round of 5 games, keeps track of the scores and declares the final 
+ * winner and score after the last round.
  * 
+ * @returns {} Prints a winner or loser announcement to console.
  */
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!.");
-    } else if (beats[humanChoice] === computerChoice) {
-        humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    /** A human and computer player choices as arguments, plays a single round, 
+     * increments the round winner's score and logs a winner announcement.
+     *   
+     * @param {string} humanChoice - human player choice.
+     * @param {string} computerChoice - computer player choice.
+     * @returns {} Prints a winner or loser announcement to console.
+     */
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = getHumanChoice().toLowerCase();
+        computerChoice = getComputerChoice();
+        const beats = { rock: "scissors", paper: "rock", scissors: "paper" };
+
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie!.");
+        } else if (beats[humanChoice] === computerChoice) {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        } else {
+            computerScore++;
+            console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`);
+        } 
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You are the WINNER! Your total score is ${humanScore}.`);
+    } else if (computerScore > humanScore) {
+        console.log(`You lost! Your lost by ${computerScore - humanScore} points.`);
     } else {
-        computerScore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-    } 
+        console.log("It's a tie! Start a new game?");
+    }
 }
 
-console.log(computerRound);
-playRound(humanRound, computerRound);
-console.log(humanScore);
-console.log(computerScore);
+playGame();
+
